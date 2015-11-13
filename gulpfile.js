4a88c279d8bb85b23e321b12d6aaad7d419fdb1c
Jaspreet Singh <js6570@yp.com>
2015-11-12 16:14:09 -0800

    Achieve 94/100 PageSpeed score from Google

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+// ///////////////////////////////////////////////
+// Required
+// ///////////////////////////////////////////////
+
+var gulp = require('gulp'),
+    minifyHTML = require('gulp-minify-html'),
+    minifyCSS = require('gulp-minify-css'),
+    clean = require('gulp-clean'),
+    imagemin = require('imagemin'),
+    imageminJPG = require('imagemin-jpegtran'),
+    pngquant = require('imagemin-pngquant');
+
+
+// ///////////////////////////////////////////////
+// Scripts
+// ///////////////////////////////////////////////
+gulp.task('img-min', function(){
+    new imagemin()
+    .src(['img/*.jpg', 'views/images/*.jpg'])
+    .dest('dist/img')
+    .use(imageminJPG())
+    .run();
+
+//    gulp.src(['img/*.jpg', 'views/images/*.jpg'])
+  //  .pipe(imagemin({use: [imageminJPG()]}))
+    //.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html-compress', function() {
+    gulp.src('*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('dist/html'));
+});
+
+gulp.task('css-minify', function(){
+    gulp.src("css/*.css")
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('clean', function(){
+    gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+
+// ///////////////////////////////////////////////
+// Default Task
+// ///////////////////////////////////////////////
+
+gulp.task('default', ['html-compress', 'css-minify', 'img-min']);
